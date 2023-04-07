e06ee047928413652ac5d4010cdbacb260214a4b
Ozeyuta <ozeyuta.jkk@gmail.com>
2023-04-07 19:20:04 +0900

    エラーメッセージの表示・バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :family_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龠々]+\z/ }
+  validates :family_name_kana, :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday, presence: true
+
 end
