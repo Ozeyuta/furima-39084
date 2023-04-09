a38f1f1687611b3aa08458f7de0e27ff1e26d746
Ozeyuta <ozeyuta.jkk@gmail.com>
2023-04-09 20:53:07 +0900

    ユーザー管理機能の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :family_name, presence: true
   validates :first_name, presence: true
-  validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龠々]+\z/ }
-  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龠々]+\z/ }
+  validates :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :family_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
