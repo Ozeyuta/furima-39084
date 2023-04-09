703914f587b5774715ba2d35d5be02fef22c0bcf
Ozeyuta <ozeyuta.jkk@gmail.com>
2023-04-09 21:29:43 +0900

    ユーザー管理機能のバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,6 @@ class User < ApplicationRecord
   validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
-  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
 
 end
