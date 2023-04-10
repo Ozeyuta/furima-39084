d9aec53d50e5571313871c10aea58c4c0cb992e0
Ozeyuta <ozeyuta.jkk@gmail.com>
2023-04-10 20:06:05 +0900

    Active_Hashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :cost
+  belongs_to_active_hash :shipping_origin
+  belongs_to_active_hash :shipping_day
 end
