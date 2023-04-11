27ba545331b040c413c2204ce69b56801380ba2c
Ozeyuta <ozeyuta.jkk@gmail.com>
2023-04-11 11:28:20 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,15 @@ class Item < ApplicationRecord
   belongs_to_active_hash :cost
   belongs_to_active_hash :shipping_origin
   belongs_to_active_hash :shipping_day
+
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :item_text, presence: true
+  validates :category, presence: true
+  validates :status, presence: true
+  validates :cost, presence: true
+  validates :shipping_origin, presence: true
+  validates :shipping_day, presence: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
 end
