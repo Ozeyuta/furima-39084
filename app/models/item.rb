34a0472ec45ee141bb56fa7faf3840912ce5fca2
Ozeyuta <ozeyuta.jkk@gmail.com>
2023-04-17 13:01:00 +0900

    バリデーションの調整

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,22 +6,16 @@ class Item < ApplicationRecord
   belongs_to :shipping_origin
   belongs_to :shipping_day
 
-  validates :image, presence: true
-  validates :item_name, presence: true
-  validates :item_text, presence: true
-  validates :category_id, presence: true
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
-  validates :status_id, presence: true
-  validates :status_id, numericality: { other_than: 1, message: "can't be blank"}
-  validates :cost_id, presence: true
-  validates :cost_id, numericality: { other_than: 1, message: "can't be blank"}
-  validates :shipping_origin_id, presence: true
-  validates :shipping_origin_id, numericality: { other_than: 1, message: "can't be blank"}
-  validates :shipping_day_id, presence: true
-  validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank"}
-  validates :price, presence: true
-  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-  validates :price, format: { with: /\A[0-9]+\z/ }
+  with_options presence: true do
+    validates :image, :item_name, :item_text,
+    validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
+    validates :status_id, numericality: { other_than: 1, message: "can't be blank"}
+    validates :cost_id, numericality: { other_than: 1, message: "can't be blank"}
+    validates :shipping_origin_id, numericality: { other_than: 1, message: "can't be blank"}
+    validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank"}
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :price, format: { with: /\A[0-9]+\z/ }
+  end
 
   belongs_to :user
   has_one_attached :image
